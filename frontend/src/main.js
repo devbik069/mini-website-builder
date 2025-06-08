25012115d2626772640f16fb98dbf0912fc5e5fd
devbik069 <bikdev069@gmail.com>
2025-06-08 23:43:20 +0545

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia';
 import axios from 'axios';
 import './assets/main.css'; 
 
-axios.defaults.baseURL = 'http://localhost:8000'; 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const app = createApp(App);
 app.use(createPinia());
